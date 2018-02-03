e3e827f3ed0b37243052966b9e6fe8e7e5992e3c
ihorfr74 <ihorfr74@gmail.com>
2018-02-03 21:59:37 +0200

    Cache invalidation without blocking the main thread

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SimplyMemoryCache
 {
@@ -8,18 +9,22 @@ namespace SimplyMemoryCache
     {
         static void Main(string[] args)
         {
-            const int cacheTimeout = 2000;
+            const int cacheLiveTime = 5000;
             CustomCache<long, string> c = new CustomCache<long, string>();
 
             for (long i = 0; i < 1000000; i++)
-                c.AddOrUpdate(i, i.ToString(), cacheTimeout);
-
-            Thread.Sleep(100);
+                c.AddOrUpdate(i, i.ToString(), cacheLiveTime);
 
             var data = c.Get(34);
-            var data34 = c.Get(34);
+            Console.WriteLine(data ?? "NULL");
 
-            Console.WriteLine(data);
+            Thread.Sleep(5000);
+            var data34 = c.Get(34);
+            Console.WriteLine(data34 ?? "NULL");
+
+
+            Thread.Sleep(1000);
+            data34 = c.Get(34);
             Console.WriteLine(data34 ?? "NULL");
 
             Console.ReadLine();
@@ -28,58 +33,45 @@ namespace SimplyMemoryCache
 
     public class CustomCache<TKey, TValue> where TValue : class
     {
-        private readonly Dictionary<TKey, TValue> data = new Dictionary<TKey, TValue>();
-        private Dictionary<TKey, DateTime> times = new Dictionary<TKey, DateTime>();
+        private readonly Dictionary<TKey, CacheObject<TValue>> data = new Dictionary<TKey, CacheObject<TValue>>();
         private readonly ReaderWriterLockSlim rwLocker = new ReaderWriterLockSlim();
-        
+
         public TValue Get(TKey key)
         {
             rwLocker.EnterReadLock();
             try
             {
-                TValue val = data.TryGetValue(key, out val) ? val : default(TValue);
-                CheckTime(key);
-                return val;
+                CacheObject<TValue> val = data.TryGetValue(key, out val) ? val : default(CacheObject<TValue>);
+
+                if (val != null)
+                {
+                    // Инвалидация кеша
+                    // Если необходимо инвалидировать весь кеш - запустить цикл с анализом невалидных ExpiredTime
+                    new Task(() =>
+                    {
+                        if (val.ExpiredTime < DateTime.Now)
+                        {
+                            Console.WriteLine("Invalidation Key = " + key + "\t Value = " + val.CacheValue + "\t Time expired:" + val.ExpiredTime);
+                            data.Remove(key);
+                        }
+                    }).Start();
+                }
+                else
+                    return null;
+
+                return val.CacheValue;
             }
             finally { rwLocker.ExitReadLock(); }
         }
 
         public TValue this[TKey key] => Get(key);
 
-        private bool CheckTime(TKey key)
-        {
-            bool isValid = false;
-            DateTime time;
-            //rwLocker.EnterReadLock();
-            try
-            {
-                if (times.TryGetValue(key, out time))
-                {
-                    if (time < DateTime.Now)
-                    {
-                        data.Remove(key);
-                        times.Remove(key);
-                        isValid = false;
-                    }
-                    else
-                    {
-                        isValid = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            return isValid;
-        }
-
-        public void AddOrUpdate(TKey key, TValue cacheObject, int timeout)
+        public void AddOrUpdate(TKey key, TValue value, int timeout)
         {
             rwLocker.EnterWriteLock();
             try
             {
+                CacheObject<TValue> cacheObject = new CacheObject<TValue>() { CacheValue = value, ExpiredTime = DateTime.Now.AddMilliseconds(timeout) };
                 if (!data.ContainsKey(key))
                 {
                     data.Add(key, cacheObject);
@@ -88,9 +80,14 @@ namespace SimplyMemoryCache
                 {
                     data[key] = cacheObject;
                 }
-                times.Add(key, DateTime.Now.AddMilliseconds(timeout));
             }
             finally { rwLocker.ExitWriteLock(); }
         }
+    }
+
+    public class CacheObject<TCacheValue> where TCacheValue : class
+    {
+        public TCacheValue CacheValue { get; set; }
+        public DateTime ExpiredTime { get; set; }
     }
 }
